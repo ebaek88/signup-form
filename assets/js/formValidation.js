40113a3c561a2dd96722718fa8e7bbe659516d41
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-03-03 21:16:59 -0700

    feat: add form validation script
    Implemented formValidation.js in parts.
    Still need to finish the implementation for password.

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -39,3 +39,60 @@ lastName.addEventListener("focusout", (evt) => {
     lastNameError.textContent = "";
   }
 });
+
+email.addEventListener("focusout", (evt) => {
+  const emailError = document.querySelector("#email + span.error");
+  if(email.validity.valueMissing) {
+    emailError.textContent = "You need to fill out your email address.";
+  } else if(email.validity.typeMismatch) {
+    emailError.textContent = "The email you entered is in a wrong format.";
+  } else {
+    emailError.textContent = "";
+  }
+});
+
+phoneNumber.addEventListener("input", (evt) => {
+  const phoneNumberError = document.querySelector("#phone-number + span.error");
+  if(phoneNumber.validity.patternMismatch) {
+    phoneNumberError.textContent = "You need to enter the number in a correct format.";
+  } else {
+    phoneNumberError.textContent = "";
+  }
+});
+
+password.addEventListener("input", (evt) => {
+  // checks for min-max length
+  if(!(password.validity.tooShort || password.validity.tooLong)) {
+    const errorText = document.querySelector(".error #char-length");
+    errorText.style.color = "#596D48";
+    errorText.textContent = "\u2705 Min 8 max 16 characters";
+  }
+
+  // need to consider else for when the character is deleted and it does not qualify again
+  // is backspace considered an "input" event?
+
+  // checks for uppercase
+  const uppercase = new RegExp("[A-Z]");
+  if(uppercase.test(password.value)) {
+    const errorText = document.querySelector(".error #uppercase");
+    errorText.style.color = "#596D48";
+    errorText.textContent = "\u2705 At least one uppercase";
+  }
+
+  // checks for a number
+  const number = new RegExp("[0-9]");
+  if(number.test(password.value)) {
+    const errorText = document.querySelector(".error #number");
+    errorText.style.color = "#596D48";
+    errorText.textContent = "\u2705 At least one number";
+  }
+
+  // checks for a special character
+  const specialCharacter = new RegExp("[^a-zA-Z0-9]");
+  if(specialCharacter.test(password.value)) {
+    const errorText = document.querySelector(".error #special-char");
+    errorText.style.color = "#596D48";
+    errorText.textContent = "\u2705 At least one special character";
+  }
+
+});
